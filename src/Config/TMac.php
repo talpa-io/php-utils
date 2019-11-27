09e034b329bb8bb3cc25970ba60d8255f4ee60ba
Jan <jan@talpa-solutions.com>
2019-11-27 09:49:06 +0100

    update

--- src/Config/TMac.php
+++ src/Config/TMac.php
@@ -13,25 +13,24 @@ use Talpa\Utils\Params\TalpaTmidParams;
 
 class TMac
 {
-    private $tmacUrl;
-    private $pathToConfigFile;
+    private $tmacHost;
+    private $localConfigPath;
 
     public function __construct(string $tmacHost, string $localConfigPath)
     {
-        $this->tmacUrl = $tmacHost;
-        $this->pathToConfigFile = $localConfigPath;
+        $this->tmacHost = $tmacHost;
+        $this->localConfigPath = $localConfigPath;
     }
 
     /**
      * Get list of all available assets
      *
      * @return array
+     * @throws
      */
     public function listAssets() : array
     {
-
-
-
+        return phore_http_request($this->tmacHost . "/v1/assets")->send()->getBodyJson()["assets"];
     }
 
 
@@ -39,10 +38,11 @@ class TMac
      * Return errors parsing config files in tmac
      *
      * @return array
+     * @throws
      */
     public function listErrors() : array
     {
-
+        return phore_http_request($this->tmacHost . "/v1/assets")->send()->getBodyJson()["errors"];
     }
 
 
@@ -52,21 +52,22 @@ class TMac
      * @param string $tmid
      * @param string|null $serviceId
      * @return array
+     * @throws
      */
     public function getConfig(string $tmid, string $serviceId=null) : array
     {
-        if(phore_file($this->pathToConfigFile)->exists()){
-            $config = phore_file($this->pathToConfigFile)->get_yaml();
+        if(phore_file($this->localConfigPath)->exists()){
+            $config = phore_file($this->localConfigPath)->get_yaml();
             try{
                 return phore_pluck(
-                    [$this->requestedService, "machines", $this->tmidParams->tmid],
+                    [$serviceId, "machines", $tmid],
                     $config,
-                    new \InvalidArgumentException("Machine " . $this->tmidParams->tmid . " is not defined in config")
+                    new \InvalidArgumentException("Machine " . $tmid . " is not defined in config")
                 );
             } catch (\InvalidArgumentException $exception){
 
             }
         }
-        return phore_http_request($this->tmacUrl)->send()->getBodyJson();
+        return phore_http_request($this->tmacHost . "/v1/assets/$tmid/$serviceId")->send()->getBodyJson();
     }
 }
